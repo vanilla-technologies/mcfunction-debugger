4554fd18fef2737a93266eba38bbe1eaa6b8aa3b
Adrodoc <adrodoc55@googlemail.com>
2021-09-01 18:55:41 +0200

    #31: Add GPL header to *.rs files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// mcfunction-debugger is a debugger for Minecraft's *.mcfunction files that does not require any
+// Minecraft mods.
+//
+// © Copyright (C) 2021 Adrodoc <adrodoc55@googlemail.com> & skess42 <skagaros@gmail.com>
+//
+// This file is part of mcfunction-debugger.
+//
+// mcfunction-debugger is free software: you can redistribute it and/or modify it under the terms of
+// the GNU General Public License as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// mcfunction-debugger is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with mcfunction-debugger.
+// If not, see <http://www.gnu.org/licenses/>.
+
 mod parser;
 mod template_engine;
 
@@ -5,7 +23,7 @@ use crate::{
     parser::{parse_line, Line},
     template_engine::TemplateEngine,
 };
-use clap::{crate_version, App, Arg};
+use clap::{crate_authors, crate_version, App, Arg};
 use futures::{future::try_join_all, FutureExt};
 use log::LevelFilter;
 use multimap::MultiMap;
@@ -53,7 +71,23 @@ async fn main() -> io::Result<()> {
             ")"
         ))
         .version_short("v")
-        .author("by Vanilla Technologies")
+        .author(&*format!(
+            "
+Vanilla Technologies
+© Copyright (C) 2021 {}
+
+mcfunction-debugger is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+mcfunction-debugger is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+",
+            crate_authors!(" & ")
+        ))
         .about("Generate debug datapacks that suspend on '# breakpoint' lines")
         .long_about(
             "Debug your datapacks in three steps:\n\
